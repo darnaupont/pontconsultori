b19e62977d19d00bf89d7b9f3ad3650b0d415e86
Damià Arnau Pont Pons <damiarnau@gmail.com>
2018-02-14 15:38:35 +0100

    mapes, contacte i links

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,64 +10,70 @@ import Foundation from 'foundation-sites';
 
 
 $(document).foundation();
-var activeLang = 'ca';
-if ($( ".lang .es" ).hasClass( "active" )) {
-  activeLang = 'es'
-};
-console.log(activeLang);
 
-var portfolioPostContainer = document.getElementById('lloopp');
-var button = document.getElementById('buttonW');
+//variables
+var pontslider = document.getElementById('pontslider');
+var metode = document.getElementById('metode');
+var lang = document.documentElement.lang;
 
-if (portfolioPostContainer) {
+//crida functions
+prova( pontslider, 'media?parent=4' , 'slider')
+prova( metode, 'pages/14' , 'post')
 
+//jquery
+$('.lang .' + lang).addClass("active");
+
+// functions
+function prova( itemID, request, tipo ){
+if (itemID) {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'http://pont.kualalumpurstudio.com/wp-json/wp/v2/media?parent=4');
+    ourRequest.open('GET', 'http://pont.kualalumpurstudio.com/wp-json/wp/v2/' + request);
     ourRequest.onload = function() {
       if (ourRequest.status >= 200 && ourRequest.status < 400) {
         var data = JSON.parse(ourRequest.responseText);
-        console.log(data);
-        // portfolioPostContainer.innerHTML =  'hola' + data.title.rendered + data.content.rendered;
-            var ourHTMLString = '';
-        for (var i = 0; i < data.length; i++) {
-          var active = '';
-          var style = '';
-          if (i == 0) {
-            active = 'active';
-            style = 'style:"position: relative; top: 0px; display: block;"';
-          }
-          ourHTMLString+=
-          '<li class="' + active + ' orbit-slide" data-slide="0"' + style +'>' +
-          '<figure class="orbit-figure">' +
-            '<img class="orbit-image" src="' + data[i].media_details.sizes.medium.source_url + '" alt="' + data[i].title.rendered +'">' +
-            '<p class="text-right">' + languageText('ca', data[i].caption.rendered) + '</p>' +
-          '</figure>' +
-          '</li>';
-          console.log(ourHTMLString);
+        if (tipo == 'slider') {
+          createslider(data, itemID);
+        } else if (tipo == 'post') {
+          createpost(data, itemID);
+        }
 
-        };
-        $(".orbit-figure").css("background-color","#BCFFB9");
-
-          portfolioPostContainer.innerHTML = ourHTMLString;
-          Foundation.reInit('orbit');
       } else {
         console.log("We connected to the server, but it returned an error.");
       }
     };
-
     ourRequest.onerror = function() {
       console.log("Connection error");
     };
     ourRequest.send();
-
 }
-function createHTML(postsData){
-    var ourHTMLString = '';
-    for (var i = 0; i < postsData.length; i++) {
-      ourHTMLString += '<h2>' + postsData[i].title.rendered + '</h2>'
-      ourHTMLString += postsData[i].content.rendered;
+}
+
+function createpost(postData, pItemID){
+  console.log(postData);
+  let ourHTMLString = '';
+  ourHTMLString = '<h1>' +  languageText(lang, postData.title.rendered) + '</h1>' + '<p>' + languageText(lang,postData.content.rendered) + '</p>';
+  pItemID.innerHTML = ourHTMLString;
+};
+
+function createslider (postsData, pItemID){
+  let ourHTMLString = '';
+  for (var i = 0; i < postsData.length; i++) {
+    var active = '';
+    var style = '';
+    if (i == 0) {
+      active = 'active';
+      style = 'style:"position: relative; top: 0px; display: block;"';
     }
-  portfolioPostContainer.innerHTML = ourHTMLString;
+    ourHTMLString+=
+    '<li class="' + active + ' orbit-slide" data-slide="0"' + style +'>' +
+    '<figure class="orbit-figure">' +
+      '<img class="orbit-image" src="' + postsData[i].media_details.sizes.medium.source_url + '" alt="' + postsData[i].title.rendered +'">' +
+      '<p class="text-right">' + languageText(lang, postsData[i].caption.rendered) + '</p>' +
+    '</figure>' +
+    '</li>';
+  };
+  pItemID.innerHTML = ourHTMLString;
+  Foundation.reInit('orbit');
   };
 
 
@@ -76,31 +82,23 @@ function createHTML(postsData){
     let caX = text.search("<:ca>");
     let EndTagX;
 
-    if ( lang == 'ca') {
-      // text CA
-      if (caX < esX) {
-      text =text.slice(caX + 5, esX);
+      if ( lang == 'ca') {
+        // text CA
+        if (caX < esX) {
+        text =text.slice(caX + 5, esX);
+        } else {
+          text = text.slice(caX + 5);
+        }
       } else {
-        text = text.slice(caX + 5);
-      }
-    } else {
-      //text no CA
-      if (caX > esX) {
-      text = text.slice(esX + 5, caX) ;
-      } else {
-        text = text.slice(esX + 5);
-      }
-    };
-
+        //text no CA
+        if (caX > esX) {
+        text = text.slice(esX + 5, caX) ;
+        } else {
+          text = text.slice(esX + 5);
+        }
+      };
 
     EndTagX = text.search("</")
     text = text.slice('EndTagX');
     return text;
-  }
-
-var orbit = new Foundation.Orbit($('#lloopp'), {animation: 'fade',
-   timerDelay: 8000,
-   pauseOnHover: true,
-   navButtons: false,
-   bullets: false
-});
+  };
